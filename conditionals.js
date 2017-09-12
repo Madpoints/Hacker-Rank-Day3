1c4adef2788a087fcdd2bcd016879f54a28dd76b
John Furtado <jf8846@gmail.com>
2017-09-12 21:58:44 +0000

    My solution to hackerRank day 3

--- conditionals.js
+++ conditionals.js
@@ -1,17 +1,19 @@
 // Task 
-// Given an integer, , perform the following conditional actions:
+// Given an integer, N, perform the following conditional actions:
 
-// If  is odd, print Weird
-// If  is even and in the inclusive range of 2 to 5, print Not Weird
-// If  is even and in the inclusive range of 6 to 20, print Weird
-// If  is even and greater than , print Not Weird
-// Complete the stub code provided in your editor to print whether or not  is weird.
+// If N is odd, print Weird
+// If N is even and in the inclusive range of 2 to 5, print Not Weird
+// If N is even and in the inclusive range of 6 to 20, print Weird
+// If N is even and greater than , print Not Weird
+// Complete the stub code provided in your editor to print whether or not N is weird.
 
 // Input Format
 
 // A single line containing a positive integer, .
 
 // Constraints
+
+// 1 <= N <= 100
 
 // Output Format
 
